443ee8de68fb72de667bcaaa83a604c85ceef2cb
Caydee <you@example.com>
2022-03-09 17:29:55 +0100

    fixes and font change

--- public/js/home.js
+++ public/js/home.js
@@ -18,7 +18,11 @@ const createBlog = (blog) => {
         <img src="${data.bannerImage}" class="blog-image" alt="">
         <h1 class="blog-title">${data.title.substring(0, 10) + '...'}</h1>
         <p class="blog-overview">${data.article.substring(0, 40) + '...'}</p>
-        <a href="/${blog.id}" class="btn dark">read</a>
+ 
+        <a href="/${blog.id}" class="read-button btn-2 hover-slide-right">
+         <span>Lexo</span>
+        </a>
+    
     </div>
     `;
 };
