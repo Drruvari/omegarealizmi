ca0556ff26489ce8ff17813bdd3819b713946d1a
Kejdi Balla <caydee>
2022-03-10 01:13:52 +0100

    colors

--- public/js/home.js
+++ public/js/home.js
@@ -15,14 +15,13 @@ const createBlog = (blog) => {
   let data = blog.data();
   blogSection.innerHTML += `
     <div class="blog-card">
+    <h1 class="blog-title">${data.title.substring(0, 10) + '...'}</h1>
         <img src="${data.bannerImage}" class="blog-image" alt="">
-        <h1 class="blog-title">${data.title.substring(0, 10) + '...'}</h1>
         <p class="blog-overview">${data.article.substring(0, 40) + '...'}</p>
  
         <a href="/${blog.id}" class="read-button btn-2 hover-slide-right">
          <span>Lexo</span>
         </a>
-    
     </div>
     `;
 };
